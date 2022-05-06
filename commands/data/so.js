31a606b26ae6bbd091e088857876d792a9b248df
ljtech <landonjohnsontechnologies@gmail.com>
2022-05-06 15:02:18 -0600

    [fix] return message on so

--- commands/data/so.js
+++ commands/data/so.js
@@ -2,10 +2,11 @@ module.exports = {
   name: "so",
   description: "Shout out a Twitch channel.",
   message: function (channel) {
-    `Go check out and follow ${channel} @ https://www.twitch.tv/${channel}!`;
+    return `Go check out and follow ${channel} @ https://www.twitch.tv/${channel}!`;
   },
   execute: function (client, user, args) {
-    if (user === "ljtechdotca") {
+    console.log({args})
+    if (user === "ljtechdotca" && args.length) {
       client.say("ljtechdotca", this.message(args[0]));
     }
   },
